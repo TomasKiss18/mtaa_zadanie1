f2d47684152b785be3a7a6088a8954b859351654
TomasKiss18 <tomaskissdk@gmail.com>
2022-03-01 23:36:33 +0100

    finálne odovzdanie
    .

---
+++ MTAA/main.py
@@ -0,0 +1,22 @@
+import myprojectSIP
+import socketserver
+import socket
+import sys
+import time
+import logging
+
+HOST, PORT = '0.0.0.0', 5060
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='proxy.log', level=logging.INFO,
+                        datefmt='%H:%M:%S')
+    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+    hostname = socket.gethostname()
+    logging.info(hostname)
+    ipaddress = socket.gethostbyname(hostname)
+    # if ipaddress == "127.0.0.1":
+    ipaddress = sys.argv[1]
+    logging.info(ipaddress)
+    myprojectSIP.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress, PORT)
+    myprojectSIP.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress, PORT)
+    server = socketserver.UDPServer((HOST, PORT), myprojectSIP.UDPHandler)
+    server.serve_forever()
